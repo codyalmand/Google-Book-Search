e62d47e1d1dc5c5cc4c5d1dbb436c8066d6beede
codyalmand <codybalmand@gmail.com>
2020-11-29 16:32:08 -0600

    Adding controllers

---
+++ src/controllers/apiController.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const axios = require("axios");
+
+module.exports={
+    findAll: function(req, res) {
+        const { query: params } = req;
+        axios
+            .get("https://www.googleapis.com/books/v1/volumes", {
+                params
+            })
+            .then(results =>
+                results.data.items.filter(
+                    result =>
+                        result.volumeInfo.title &&
+                        result.volumeInfo.authors &&
+                        result.volumeInfo.description &&
+                        result.volumeInfo.imageLinks &&
+                        result.volumeInfo.imageLinks.thumbnail 
+                )
+            )
+            .then(apiBooks =>
+                db.Book.find().then(dbBooks => 
+                    apiBooks.filter(apiBook =>
+                        dbBooks.every(dbBook => dbBook.google.Id.toString() !== apiBook.id)
+                    )
+
+                )
+            )
+            .then(books => res.json(books))
+            .catch(err => res.status(422).json(err));
+    }
+};
