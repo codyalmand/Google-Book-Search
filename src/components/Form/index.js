0f8040f760d0975fc87ea8a8a325db177e18a906
codyalmand <codybalmand@gmail.com>
2020-11-29 17:00:19 -0600

    Added styling to components

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./style.css"
 
 function Form(props){
 return (
   <div className="container">
   <form>
     <div className="form-group">
-      <label htmlFor="search"><h2>Search for and save Books of Interest</h2></label>
+      <label id="searchtitle" htmlFor="search"><h2>Search For Books / Save Books For Later</h2></label>
       <input
         onChange={props.handleInputChange}
         value={props.search}
--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import "./style.css"
 
 class Results extends Component {
 
@@ -30,7 +31,7 @@ class Results extends Component {
         return (
             <div>
                 {!this.props.books.length ? (
-                    <h1 className="text-center">No Results to Display</h1>
+                    <h1 id="noresults" className="text-center">No Results to Display</h1>
                 ) : (
                         <div>
                             {this.props.books.map(result => (
