079557bb6f38b31c1169fafe6e3c45ef664593a6
codyalmand <codybalmand@gmail.com>
2020-11-30 14:45:42 -0600

    Changed NoMatch page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -5,8 +5,8 @@ function NoMatch() {
     <div>
             <h1>404 Page Not Found</h1>
             <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
+              <span>
+                Error 404
               </span>
             </h1>
     </div>
